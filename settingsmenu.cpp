e2a8877049730ba19916bf69ce98a92f57f43f1e
Marko Ranta <marko.ranta95@gmail.com>
2015-06-22 01:41:26 +0300

    Settings menu has now one fully working tab (appearance), and it can now affect to editors settings.

--- settingsmenu.cpp
+++ settingsmenu.cpp
@@ -1,14 +1,46 @@
 #include "settingsmenu.hpp"
 #include "ui_settingsmenu.h"
+#include <QCloseEvent>
+#include "settings.hpp"
 
 SettingsMenu::SettingsMenu(QWidget *parent) :
 	QDialog(parent),
 	ui(new Ui::SettingsMenu)
 {
 	ui->setupUi(this);
+	setHidden(true);
+
 }
 
-SettingsMenu::~SettingsMenu()
-{
+SettingsMenu::~SettingsMenu() {
 	delete ui;
+}
+
+AppearanceTab* SettingsMenu::appearanceTab()
+{
+	return ui->appearance;
+}
+
+
+void SettingsMenu::on_okButton_clicked() {
+	on_applyButton_clicked();
+	setHidden(true);
+}
+
+void SettingsMenu::on_applyButton_clicked() {
+	ui->appearance->updateSyntaxStyle();
+	Settings::set("TextEditor/Font", ui->appearance->mFont.family());
+	Settings::set("TextEditor/FontSize", ui->appearance->mFont.pointSize());
+	Settings::sync();
+	emit settingsApplied();
+}
+
+void SettingsMenu::on_cancelButton_clicked() {
+	ui->appearance->useSyntaxStyle();
+	setHidden(true);
+}
+
+void SettingsMenu::closeEvent(QCloseEvent* e) {
+	on_cancelButton_clicked();
+	e->accept();
 }
--- settingsmenu.hpp
+++ settingsmenu.hpp
@@ -2,21 +2,34 @@
 #define SETTINGSMENU_HPP
 
 #include <QDialog>
+#include "appearancetab.hpp"
 
 namespace Ui {
 	class SettingsMenu;
 }
 
-class SettingsMenu : public QDialog
-{
+class SettingsMenu : public QDialog {
+		friend class Colorscheme;
 		Q_OBJECT
+		Ui::SettingsMenu* ui;
 
 	public:
 		explicit SettingsMenu(QWidget *parent = 0);
 		~SettingsMenu();
 
-	private:
-		Ui::SettingsMenu *ui;
+		AppearanceTab* appearanceTab();
+
+		void closeEvent(QCloseEvent *);
+	signals:
+		void settingsApplied();
+
+	private slots:
+
+		void on_okButton_clicked();
+
+		void on_applyButton_clicked();
+
+		void on_cancelButton_clicked();
 };
 
 #endif // SETTINGSMENU_HPP
